4b481b5a5ec0ae7bc8d57ad9bd49cfa1410661dc
Video Generator Dev <developer@videogen.com>
2025-10-15 08:25:17 +0200

    Deploy to GitHub Pages

--- android/app/src/main/kotlin/com/example/video_generator_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.video_generator_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/subsera/app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.subsera.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
